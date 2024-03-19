ea4adbdaa2c5b89ec1da1a137ce9391b14185c3b
Man-Q <81616738+Man-Q@users.noreply.github.com>
2024-03-19 18:13:41 +0200

    Proper naming for my messages

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ console.log(`${preferredDrink}`);
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
-let message = (cansLeft < 5) ? "Time to restock!" : "We're stocked!";
-console.log(`${message}`);
+let message1 = (cansLeft < 5) ? "Time to restock!" : "We're stocked!";
+console.log(`${message1}`);
 
 
 // #### Challenge 3: Workout Intensity
